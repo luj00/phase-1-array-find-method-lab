16d3eee2b4d3c79694fe37879e04a75fef027e49
root <root@DESKTOP-598GE48.localdomain>
2023-10-07 15:08:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    // Use the find method to search for a Denver Broncos win
+    const broncosWin = record.find(win => win.team === 'Denver Broncos' && win.year === '2015');
+  
+    // If a Broncos win in 2015 is found, return the year '2015', otherwise return undefined
+    return broncosWin ? '2015' : undefined;
+  }
+  
+  
